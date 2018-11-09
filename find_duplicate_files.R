d20fce365be2534e6760da3901d10710707839e3
wooii <33645620+wooii@users.noreply.github.com>
2018-11-09 17:23:00 +1000

    Create find_duplicate_files.R

---
+++ find_duplicate_files.R
@@ -0,0 +1,17 @@
+
+find_duplicate_files <- function(x) {
+  # Find the duplicate files in a folder, i.e. the files with the same md5 vlues.
+  # x: a string of file folder, e.g. "C:/example_folder"
+  # Return: a list of duplicate files.
+
+  f <- dir(x, recursive = T, all.files = T, full.names = T) # file dir
+  f_md5 <- sapply(f, tools::md5sum, USE.NAMES = T) # file md5 values
+  f_grouped <- split(f, f_md5) # Group f by f_md5. 
+  
+  # Divide the list into duplicates (length > 1) and uniques (length = 1).
+  d_i <- sapply(f_grouped, function(x) length(x) > 1) # index of the duplicate files
+  y <- f_grouped[d_i]
+  return(y)
+}
+
+d_files <- find_duplicate_files(x = "E:/iPhone 7")
